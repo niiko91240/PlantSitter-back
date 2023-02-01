5d1056b5d992d81275e7a70e565e30e3881e4851
niiko fx <nicolas.ajrd@gmail.com>
2023-02-01 13:32:38 +0100

    Modif url Swagger

--- PlantSitter/PlantSitter/urls.py
+++ PlantSitter/PlantSitter/urls.py
@@ -20,5 +20,5 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apit/', include('PlantSitterApp.urls')),
-   #re_path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
