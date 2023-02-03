1483e48feab6e7c730cb3fcdb515b85199e08cfb
niiko fx <nicolas.ajrd@gmail.com>
2023-02-03 10:01:44 +0100

    Modification image conseil

--- PlantSitter/PlantSitterApp/modeles/conseil.py
+++ PlantSitter/PlantSitterApp/modeles/conseil.py
@@ -4,5 +4,5 @@ from .plante import Plante
 class Conseil(models.Model):
     titre = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.URLField()
+    image = models.URLField(blank=True, null=True)
     idPlante = models.ForeignKey(Plante, on_delete=models.CASCADE, related_name='plante')
