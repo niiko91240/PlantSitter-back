f26b4fff7412f37148e30dd8a94797d768dd53f5
Nicolas Anjorand <nicolas.ajrd@gmail.com>
2023-06-20 10:59:30 +0200

    restore utilisateur

--- PlantSitter/PlantSitterApp/vues/utilisateur.py
+++ PlantSitter/PlantSitterApp/vues/utilisateur.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from django.http import HttpResponse
+from rest_framework.response import Response
 
 from ..serializers import UtilisateurSerializer
 from ..modeles.utilisateur import Utilisateur
@@ -6,3 +8,12 @@ from ..modeles.utilisateur import Utilisateur
 class UtilisateurViewSet(viewsets.ModelViewSet):
    queryset = Utilisateur.objects.all()
    serializer_class = UtilisateurSerializer
+
+   def create(self, request, *args, **kwargs):
+      mail = request.data['id']
+      mdp = request.data['mdp']
+      try:
+         utilisateur = Utilisateur.objects.get(login=mail, mdp=mdp)
+         return HttpResponse(utilisateur.id,status=201)
+      except:
+         return HttpResponse(status=400)
